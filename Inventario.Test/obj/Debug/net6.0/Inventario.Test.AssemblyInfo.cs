84d1775add274f3aa1225d5dc4bf606821772e26
leoneldavidalvarez1-dot <leoneldavidalvarez1@gmail.com>
2026-05-07 11:27:22 -0500

    Update generated build metadata and paths
    Regenerate build artifacts: update AssemblyInformationalVersion for Inventario and Inventario.Test to include the commit identifier (+725f1c6...), refresh various generated asset/cache files and CoverletSourceRootsMapping, and remove Inventario.Test.csproj.AssemblyReference.cache. Also update GeneratedMSBuildEditorConfig ProjectDir entries to reflect a different local user path (SENA -> Alex). These are automated changes from a rebuild on a different machine/user environment.

--- Inventario.Test/obj/Debug/net6.0/Inventario.Test.AssemblyInfo.cs
+++ Inventario.Test/obj/Debug/net6.0/Inventario.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725f1c676cae4842ddb345d95e22cd2ffba99737")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Inventario/obj/Debug/net6.0/Inventario.AssemblyInfo.cs
+++ Inventario/obj/Debug/net6.0/Inventario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725f1c676cae4842ddb345d95e22cd2ffba99737")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
