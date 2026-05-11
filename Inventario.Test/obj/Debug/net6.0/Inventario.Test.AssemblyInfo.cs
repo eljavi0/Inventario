581f948740864e969a3065be07117d33e9ac1148
eljavi0 <javier_medrano@soy.sena.edu.co>
2026-05-10 19:16:48 -0500

    Update generated build artifacts and local paths

--- Inventario.Test/obj/Debug/net6.0/Inventario.Test.AssemblyInfo.cs
+++ Inventario.Test/obj/Debug/net6.0/Inventario.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2537703b98760b22f60d7b49c459af66bfdda6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d3f3b3efcf3526b00bafc3fd1b00e1e632f5d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
